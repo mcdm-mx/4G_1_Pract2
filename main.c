624daaa686dc975bfb306bb3a6544acd6e0442b9
Alberttoaguayo <aaguayo2@ucol.mx>
2018-04-11 15:47:12 -0500

    Prototipo de rutina de error y rutina de error

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
 #use delay(clock=16000000)
 
-#define __DEBUG_SERIAL__ //Si comentas esta linea se deshabilita el debug por serial y el PIN_C6 puede ser usado en forma digital I/O
+//#define __DEBUG_SERIAL__ //Si comentas esta linea se deshabilita el debug por serial y el PIN_C6 puede ser usado en forma digital I/O
 
 #ifdef __DEBUG_SERIAL__
    #define TX_232        PIN_C6
@@ -11,6 +11,7 @@
    #use fast_io(c)
 #endif
 
+void rutinaDeError();
 void main (void){
    setup_oscillator(OSC_16MHZ);
 #ifdef __DEBUG_SERIAL__ //Deberiamos de proteger nuestras depuraciones de esta forma o usar una macro ya protegida.
@@ -20,3 +21,16 @@ void main (void){
       
    }
 }	
+void rutinaDeError(){
+    for(int i = 0 ; i < 5 ; i++){
+        output_a(0xFF);
+        output_b(0x0F);
+        output_e(0x07);
+        delay_ms(150);
+        output_a(0x00);
+        output_b(0x00);
+        output_e(0x00);
+        delay_ms(150);
+    }
+}
+
