7fe05797e1961bb1c1d601e71e11950641b993a7
HugoPerez5 <vperezh447@gmail.com>
2018-04-11 15:24:41 -0500

    Configuracion de puertos de entrada y salida. Decalracion de variables

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
 #use delay(clock=16000000)
 
-#define __DEBUG_SERIAL__ //Si comentas esta linea se deshabilita el debug por serial y el PIN_C6 puede ser usado en forma digital I/O
+//#define __DEBUG_SERIAL__ //Si comentas esta linea se deshabilita el debug por serial y el PIN_C6 puede ser usado en forma digital I/O
 
 #ifdef __DEBUG_SERIAL__
    #define TX_232        PIN_C6
@@ -16,6 +16,13 @@ void main (void){
 #ifdef __DEBUG_SERIAL__ //Deberiamos de proteger nuestras depuraciones de esta forma o usar una macro ya protegida.
    printf("Hola Mundo\n");//Puedes usar putc o printf. Revisa la documentación de CCS para ver que mas puedes hacer.
 #endif
+   set_tris_a(0xC0);
+   set_tris_b(0xF0);
+   set_tris_c(0xFF);
+   set_tris_d(0xFF);
+   set_tris_e(0x08);
+   int opcion;
+   signed long resultado = 0x00;
    while(1){
       
    }
